dc87aca524d54251d4348c5402914ed436a15bdc
Christopher Blackford <christopher.blackford@mail.utoronto.ca>
2018-09-24 13:27:52 -0400

    Starting Atlantic Canada connectivity with Sam

---
+++ Creating hexagon study extent.R
@@ -0,0 +1,47 @@
+#Clear workspace
+rm(list=ls())
+
+#Clipping to your study extent
+EEZ <- readOGR("./shapefiles", "")
+
+#Loading Remi's grid where larvae were released
+grid <- readOGR("./shapefiles", "")
+NAD_projection <- proj4string(grid)
+
+#Dissolve into one polygon since so you can change grid dimensions
+grid <- spTransform(grid, EEZ@proj4string)
+grid <- gUnaryUnion(grid)
+
+#Intersecting - don't know why this works and ConPoly2 <- grid[EEZ,] doesn't
+ConPoly <- gIntersection(grid, EEZ, byid = FALSE, drop_lower_td = TRUE) #This works, but you'll have to choose a shapefile that includes islands and doesn't cut-off at rivers 
+
+#Adding dataframe so you can create a shapefile of new study extent
+ConPoly_ID <- 1 
+ConPoly <- SpatialPolygonsDataFrame(ConPoly, as.data.frame(ConPoly_ID))
+
+writeOGR(ConPoly, dsn = "", layer = "EEZ_hexagons", 
+         driver = "ESRI Shapefile", verbose = TRUE, overwrite = TRUE, morphToESRI = TRUE)
+
+
+#Creating the hexagon layer
+ConPoly_hex <- ConPoly
+
+hex_area <- my_resolution^2
+dist_between_centroids <- sqrt(2*hex_area/sqrt(3))
+
+ConPoly_hex = gBuffer(ConPoly_hex, width = 0)
+HexPts <- spsample(ConPoly_hex, type="hexagonal", cellsize = dist_between_centroids, offset = c(0, 0))
+HexPols <- HexPoints2SpatialPolygons(HexPts)
+plot(HexPols)
+
+row.names(HexPols) <- as.character(1:length(HexPols))
+Poly_ID <- 1:length(HexPols)
+
+HexPols <- SpatialPolygonsDataFrame(HexPols, as.data.frame(Poly_ID))
+ConPoly <- HexPols
+
+rm(grid, ConPoly_ID, EEZ, hex_area, dist_between_centroids, Poly_ID, ConPoly_hex, HexPts, HexPols)
+
+writeOGR(ConPoly, dsn = "./cuke_present/StudyExtent/Inshore_extent_grid", layer = "Hexagon_grid", 
+         driver = "ESRI Shapefile", verbose = TRUE, overwrite = TRUE, morphToESRI = TRUE)
+
