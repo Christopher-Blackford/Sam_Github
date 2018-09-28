a4f7f2a8e219af1bcc61c43cb538e318f86692e4
Christopher Blackford <christopher.blackford@mail.utoronto.ca>
2018-09-28 16:20:49 -0400

    Added shapefiles and stuff

--- Creating hexagon study extent.R
+++ Creating hexagon study extent.R
@@ -1,32 +1,40 @@
 #Clear workspace
 rm(list=ls())
 
+library(rgdal)
+library(rgeos)
+library(sp)
+
 #Clipping to your study extent
-EEZ <- readOGR("./shapefiles", "")
+EEZ <- readOGR("./shapefiles/Atlantic_eez", "Atlantic_eez")
 
 #Loading Remi's grid where larvae were released
-grid <- readOGR("./shapefiles", "")
-NAD_projection <- proj4string(grid)
+#grid <- readOGR("./shapefiles", "")
+#NAD_projection <- proj4string(grid)
 
 #Dissolve into one polygon since so you can change grid dimensions
-grid <- spTransform(grid, EEZ@proj4string)
-grid <- gUnaryUnion(grid)
+#grid <- spTransform(grid, EEZ@proj4string)
+#grid <- gUnaryUnion(grid)
 
 #Intersecting - don't know why this works and ConPoly2 <- grid[EEZ,] doesn't
-ConPoly <- gIntersection(grid, EEZ, byid = FALSE, drop_lower_td = TRUE) #This works, but you'll have to choose a shapefile that includes islands and doesn't cut-off at rivers 
+#ConPoly <- gIntersection(grid, EEZ, byid = FALSE, drop_lower_td = TRUE) #This works, but you'll have to choose a shapefile that includes islands and doesn't cut-off at rivers 
+
 
 #Adding dataframe so you can create a shapefile of new study extent
-ConPoly_ID <- 1 
-ConPoly <- SpatialPolygonsDataFrame(ConPoly, as.data.frame(ConPoly_ID))
+#ConPoly_ID <- 1 
+#ConPoly <- SpatialPolygonsDataFrame(ConPoly, as.data.frame(ConPoly_ID))
 
-writeOGR(ConPoly, dsn = "", layer = "EEZ_hexagons", 
-         driver = "ESRI Shapefile", verbose = TRUE, overwrite = TRUE, morphToESRI = TRUE)
+#writeOGR(ConPoly, dsn = "", layer = "EEZ_hexagons", 
+ #        driver = "ESRI Shapefile", verbose = TRUE, overwrite = TRUE, morphToESRI = TRUE)
 
+ConPoly <- EEZ
 
 #Creating the hexagon layer
 ConPoly_hex <- ConPoly
 
-#hex_area <- #Need to define how large (in m2) you want hexagons to be
+#0.25 degrees is approx. 20 horizontal X 28 vertical km = 560 km2
+hex_area_km <- 560
+hex_area <- hex_area_km*1000000
 dist_between_centroids <- sqrt(2*hex_area/sqrt(3)) #This formula converts between interior hexagon area which is what we are interested in and distance between hexagon centroids which R uses to define hexagon layer
 
 ConPoly_hex = gBuffer(ConPoly_hex, width = 0)
