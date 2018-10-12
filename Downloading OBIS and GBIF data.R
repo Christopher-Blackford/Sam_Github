8d9536c1fc098d7e76bcd4fad803d9e1f47560bf
Christopher Blackford <christopher.blackford@mail.utoronto.ca>
2018-10-12 19:47:43 -0400

    gettin obis data

--- Downloading OBIS and GBIF data.R
+++ Downloading OBIS and GBIF data.R
@@ -1,13 +1,14 @@
-#install.packages("devtools")
+
+###Loading packages
 library(devtools)
 #install_github("iobis/robis")
 library(robis)
 library(tidyverse)
-install.packages('rgbif')
 library(rgbif)
-install.packages("ff")
-library(ff)
-setwd("C:/Users/coleb/Dropbox/2018MJFWorkStudy/ChrisConnectivityWork")
+#library(ff)
+
+getwd()
+
 #######OBIS data download
 obisdata <- occurrence(geometry = 
 "POLYGON ((-69.14795 44.02442, -67.71973 41.45920, 
@@ -15,9 +16,18 @@ obisdata <- occurrence(geometry =
 -46.31836 50.62507, -48.60352 54.74999, -53.65723 58.63122, 
 -63.32520 59.04055, -73.87207 46.76997, -69.14795 44.02442))")
 
-smallerobisdata <- occurrence(geometry = 
-"POLYGON ((-60.6775 47.3463, -59.6997 46.3697, -57.7441 46.9353, 
--59.8975 47.7984, -60.6775 47.3463))") 
+df1 <- obisdata[c(1:1000000),]
+write.csv(df1, "./output/LargeData/df1.csv")
+
+df2 <- obisdata[c(1000001:2000000),]
+write.csv(df2, "./output/LargeData/df2.csv")
+
+df3 <- obisdata[c(2000001:3901265),]
+write.csv(df3, "./output/LargeData/df3.csv")
+
+
+
+
 
 smallerobisdatavars <- c('id',
                   'decimalLongitude',
@@ -74,6 +84,12 @@ obisdata <- read.csv.ffdf(file = "file:///C:/Users/coleb/Dropbox/2018MJFWorkStud
 
 #######RGBIF data download
 
+occ_download(geometry = "POLYGON ((-60.6775 47.3463, -59.6997 46.3697, -57.7441 46.9353, 
+-59.8975 47.7984, -60.6775 47.3463))")
+
 occ_download_import(x = NULL, key = NULL, 
                     path = "C:\Users\coleb\Dropbox\2018MJFWorkStudy\ChrisConnectivityWork\OBIS\0003507-181003121212138.zip", fill = FALSE)
+
+
+
 
