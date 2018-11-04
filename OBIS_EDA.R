2cd9f5960f63389c9ef86d63ff24f145be473785
colebrookson <cole.brookson@mail.utoronto.ca>
2018-11-04 10:28:45 -0500

    New Plots and New code for additional groupings
    Did some data manipulation to make groupings for cod, capelin and the seabirds. See extensive notes on what I did to determine which birds count as 'seabirds', obviously this can be changed as you see fit, but there are two different bird ones, for reasons explained in the code. Overall: This will allow for heatmaps with capelin, cod and seabirds

--- OBIS_EDA.R
+++ OBIS_EDA.R
@@ -17,6 +17,7 @@ memory.limit(size = 25000)
 ########################
 # Loading in OBIS files and clipping to study extent (Chris)
 
+
 filenames <- list.files(path="LargeData/OBIS/", pattern= "OBISData_", full.names=TRUE, recursive=T)
 
 #datalist <- lapply(filenames, read.csv) # load all files into a list - slower than my loop?
@@ -26,7 +27,7 @@ for (i in 1:length(filenames)){temp <- read.csv(filenames[i]); OBIS <- rbind(OBI
 rm(temp)
 
 names(OBIS)
-obisnames<- c('id', 'class', 'phylum', 'family', 'order', 'species', 'decimalLongitude', 'decimalLatitude', 'obisID', 'countryCode', 'catalogNumber')
+obisnames<- c('yearcollected','genus', 'id', 'class', 'phylum', 'family', 'order', 'species', 'decimalLongitude', 'decimalLatitude', 'obisID', 'countryCode', 'catalogNumber')
 OBIS <- OBIS[obisnames]
 
 ###Spatial subset based on being within study extent
@@ -57,7 +58,7 @@ rm(OBIS_observations, xy, i, filenames, obisnames)
 #Step: filter arachnids and insects
 OBIS <- OBIS %>% 
   filter(class != "Insecta", class != 'Arachnida') 
-
+summary(OBIS$yearcollected)
 
 #Step: make a few dataframes
 OBIS_Arth <- OBIS %>% 
@@ -147,4 +148,136 @@ OBIS_Poly <- sp::merge(Study_Area_hex, Polydf)
 
 writeOGR(OBIS_Poly, dsn = "./output/shapefiles/OBIS", layer = "OBIS_Poly", driver = "ESRI Shapefile", 
          verbose = TRUE, overwrite = TRUE, morphToESRI = TRUE)
+
+########################
+########################
+
+
+###############Subset dataframe into 5 year blocks
+summary(OBIS$yearcollected)
+
+#use the floor function to round down, along with the mutate function which is adding a new column
+
+OBIS <- OBIS %>% 
+  mutate(year5 = floor(yearcollected / 5) * 5)
+
+############### New Heat Maps for Capelin, Cod, and Seabirds
+names(OBIS)
+## New data frames for Capelin, cod and seabirds
+CodOBIS <- OBIS %>% 
+       filter(species == 'Gadus morhua')
+CapelinOBIS <- OBIS %>%
+        filter(species == 'Mallotus villosus')
+#see below about the two seabird dataframes
+consbirdsOBIS <- OBIS %>% 
+        filter(class == 'Aves', 
+               !family %in% c('Cathartidae','Corvidae','Falconidae','Fringillidae', 
+                 'Emberizidae','Hirundinidae','Icteridae','Parulidae','Troglodytidae',
+                 'Alaudidae','Apodidae','Caprimulgidae','Cardinalidae','Cuculidae',
+                 'Mimidae','Turdidae','Tyrannidae'))
+libbirdsOBIS <- OBIS %>% 
+  filter(class == 'Aves', 
+         !family %in% c('Anatidae','Phalaropes','Phalaropodidae','Accipitridae','Cathartidae','Ardeidae','Corvidae','Falconidae',
+                        'Fringillidae', 'Podicipedidae','Recurvirostridae', 'Scolopacidae',
+                        'Threskiornithidae', 'Phaethontidae','Aramidae',
+                        'Emberizidae','Hirundinidae','Icteridae','Parulidae','Troglodytidae',
+                        'Alaudidae','Apodidae','Caprimulgidae','Cardinalidae','Cuculidae',
+                        'Mimidae','Turdidae','Tyrannidae'))
+
+
+# Based on the info below, I excluded a number of these families, I made two 
+#dataframes, a conservative one with as many reasonable taxa as possible,
+#and a liberal one with lots excluded, the ones I excluded or included
+#are obvious in the code
+
+# Some Notes on the Different Families:
+    # Alcidae is the auks
+    # Anatidae is the ducks and swans
+    # Gaviidae is the loons
+    # Hydrobatidae is the northern storm petrel
+    # Laridae is the gulls
+    # Phalacrocoracidae is the cormorants
+    # Phalaropodidae is shorebirds 
+    # Procellariidae includes petrels and the like 
+    # Stercoraridae is the skuas
+    # Sternidae is the terns
+    # Sulidae is the gannetts and boobies
+#   Phalaropes is shorebirds
+#   Accipitridae is hawks and eagles and such, not quite seabirds?
+#   Ardeidae is the herons
+#   Cathartidae is vultures/condors
+#   Charadriidae is plovers etc. (shorebirds)
+#   Corvidae a family of songbirds (jays)
+#   Falconidae is the falcons
+#   Fringillidae is finches
+#   Podicipedidae is the grebes
+#   Recurvirostridae is wading shorebirds
+#   Scolopacidae is sandpipers
+#   Threskiornithidae is spoonbills (waders)
+#   Emberizidae is a group of passerine songbirds
+#   Hirundinidae is the swallows
+#   Icteridae is passerine songbirds
+#   Parulidae is warblers
+#   Phaethontidae are the tropicbirds, a group of seabirds, but are native to the tropics
+#   Troglodytidae are wrens
+#   Alaudidae are larks
+#   Apodidae are swifts
+#   Aramidae - monotypic taxa thats a tropical shorebird
+#   Caprimulgidae is the nightjars
+#   Cardinalidae is cardinals
+#   Cuculidae is the cuckoos
+#   Fregatidae is a tropical and sub-tropical seabird 
+#   Gruidae is the cranes
+#   Mimidae is a group of songbirds
+#   Turdidae is the thrushes
+#   Tyrannidae is the largest group od songbirds
+
+## add columns to OBIS dataframe for each bird grouping to allow for easy addition to the PolyDf
+OBIS <- OBIS %>% 
+  mutate(., LibBird = ifelse(family %in% c('Alcidae', 'Gaviidae','Hydrobatidae',
+                                           'Laridae','Phalacrocoracidae','Procellariidae',
+                                           'Stercoraridae','Sternidae','Sulidae'), 'Lib', 'No'))
+
+OBIS <- OBIS %>% 
+  mutate(., ConsBirds = ifelse(family %in% c('Alcidae', 'Anatidae','Gaviidae','Hydrobatidae',
+                                             'Laridae','Phalacrocoracidae','Phalaropodidae','Procellariidae',
+                                             'Stercoraridae','Sternidae','Sulidae','Phalaropes','Ardeidae','Charadriidae',
+                                             'Podicipedidae','Recurvirostridae','Scolopacidae','Threskiornithidae',
+                                             'Phaethontidae','Aramidae','Fregatidae'), 'Cons', 'No'))
+
+
+
+## Plot for Seabirds
+consbirdsOBISp1 <- consbirdsOBIS %>% 
+  group_by(family) %>% 
+  ggplot(aes(x = family, na.rm = TRUE)) +
+  geom_bar(colour = 'black', fill = 'red2') +
+  theme_classic()
+consbirdsOBISp1 <- consbirdsOBISp1 + theme(axis.text.x = element_text(angle = 90, size = 5))
+consbirdsOBISp1
+
+libbirdsOBISp1 <- libbirdsOBIS %>% 
+  group_by(family) %>% 
+  ggplot(aes(x = family, na.rm = TRUE)) +
+  geom_bar(colour = 'black', fill = 'red2') +
+  theme_classic()
+libbirdsOBISp1 <- libbirdsOBISp1 + theme(axis.text.x = element_text(angle = 90, size = 5))
+libbirdsOBISp1
+
+########################
+########################
+#Add the capelin, both seabirds, and cod to the polydf dataframe so we have counts per polygon
+
+
+Polydf <- OBIS %>% 
+  group_by(., Poly_ID) %>% 
+  summarize(., Chordates = sum(phylum == 'Chordata'),
+            Arthropods = sum(phylum == 'Arthropoda'),
+            AllElse = sum(phylum != 'Chordata' & phylum != 'Arthropoda'), 
+            Cod = sum(species == 'Gadus morhua', na.rm = TRUE),
+            Capelin = sum(species == 'Mallotus villosus', na.rm = TRUE),
+            ConsBirds = sum(ConsBirds == 'Cons'),
+            LibBird = sum(LibBird == 'Lib'))
+
+
 
